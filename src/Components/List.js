8b6939a69a8d1d25c1865c876e49f13c786daac1
Vishal Sarvagod <vishalsarvagod91@gmail.com>
2021-02-16 18:56:54 -0500

    Accordions Added

---
+++ src/Components/List.js
@@ -0,0 +1,17 @@
+import React, { useState } from 'react';
+
+const List = ()=>{
+    const [term,setTerm] = useState('');
+
+    return (
+        <div>
+               <div className="ui form">
+                   <div className="field">
+                       <label>Enter Search Term</label>
+                       <input className="input" value={term} onChange={e=>setTerm(e.target.value)}/>
+                   </div>
+               </div>
+        </div>
+    );
+};
+export default List;
