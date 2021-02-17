1da33b71360490d1972fed5b1335a6e1ed1d3831
Vishal Sarvagod <vishalsarvagod91@gmail.com>
2021-02-17 13:06:49 -0500

    Wiki api added

--- src/Components/List.js
+++ src/Components/List.js
@@ -1,7 +1,48 @@
-import React, { useState } from 'react';
-
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
 const List = ()=>{
     const [term,setTerm] = useState('');
+    const [results,setResults] = useState([]);
+    console.log(results);
+    useEffect(()=>{
+        const search = async() =>{
+            const {data} = await axios.get('https://en.wikipedia.org/w/api.php',{
+                params:{
+                    action: 'query',
+                    list: 'search',
+                    origin: '*',
+                    format: 'json',
+                    srsearch: term,
+                }
+            });
+            setResults(data.query.search);
+        };
+        const timeoutId = setTimeout(()=>{
+            if(term){
+                search();
+            }
+        },500); 
+        return () =>{
+            clearTimeout(timeoutId);
+        };      
+    },[term]);
+
+    const renderedResults = results.map((result)=>{
+        return (
+            <div key={result.pageid} className="item">
+                <div className="right floated content">
+                    <a className="ui button" href={`https://en.wikipedia.org?curid=${result.pageid}`}>Go</a>
+                </div>
+                <div className="content">
+                    <div className="header">
+                        {result.title}
+                    </div>
+                    <span dangerouslySetInnerHTML={{__html: result.snippet}}></span>
+                </div>
+
+            </div>
+        )
+    });
 
     return (
         <div>
@@ -10,6 +51,9 @@ const List = ()=>{
                        <label>Enter Search Term</label>
                        <input className="input" value={term} onChange={e=>setTerm(e.target.value)}/>
                    </div>
+               </div>
+               <div className="ui celled list">
+                   {renderedResults}
                </div>
         </div>
     );
